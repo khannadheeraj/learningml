9e482a95e86067ef95a24e52e75ccfd080907ec7
Koushik Bag <koushik.bag@wseinfratech.com>
2026-05-27 22:27:24 +0530

    implement Invitation Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SendInvitationDashboard from '../components/Invitation';
 
 const Dashboard = React.lazy(() => import('../components/Dashboard/index'))
 const LogOut = React.lazy(() => import('../components/LogOut/index'))
@@ -9,6 +10,7 @@ const routes = [
     { path: '/', exact: true, name: 'Home', element: <Dashboard /> },
     { path: '/dashboard', name: 'Dashboard', element: <Dashboard /> },
     { path: '/logout', name: 'Logout', element: <LogOut /> },
+    { path: '/send-invitation', name: 'Send Invitation', element: <SendInvitationDashboard /> },
 
 ]
 
